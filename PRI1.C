465ce7b6d9ef29e413a7a0724ebd833c6b4b7dd8
priyankapatel1234 <55329362+priyankapatel1234@users.noreply.github.com>
2020-02-02 10:39:51 -0800

    Add files via upload

---
+++ PRI1.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ void main()
+ {
+  char ch;
+  printf("enter a character");
+//  ch=getchar();
+//ch=getch();
+//ch=getche();
+      scanf("%c",&ch);
+  printf("%d",ch);
+  getch();
+  }
