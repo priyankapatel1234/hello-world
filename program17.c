2d4e63cf837daff6116b8d983c5c546609026bc2
priyankapatel1234 <55329362+priyankapatel1234@users.noreply.github.com>
2020-03-05 08:46:20 -0800

    Add files via upload

---
+++ program17.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+switch(printf("IIT"))
+{
+default:printf("guwahati");
+case 1:printf("delhi");
+break;
+case2:
+printf("kharagpur");
+break;
+case3:printf("madras");
+break;
+}
+return 0;
+}
