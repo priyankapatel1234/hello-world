16d8a961d4b9627e668af1e82bb1eaebd267e6b3
priyankapatel1234 <55329362+priyankapatel1234@users.noreply.github.com>
2020-03-05 08:45:49 -0800

    Add files via upload

---
+++ program16.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+unsigned long long result=1;
+printf("enter any integer:");
+scanf("%d",&n);
+for(i=1;i<=n;++i)
+{
+result *=i;
+
+}
+printf("the output of the program is %llu",result);
+return 0;
+}
